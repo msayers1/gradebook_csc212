d17bbd1d7e8ce1d5afbfa208dfc7a34c27bbdc02
Michael Sayers <msayers1@gmail.com>
2022-10-17 19:44:22 -0400

    -

--- main.cpp
+++ main.cpp
@@ -21,15 +21,14 @@ int main(int argc, char* argv[]){
     while(running == true){
         std::cout << "\nPress 1 to print coursework." << std::endl;
         std::cout << "Press 2 to print a category grade." << std::endl;
-        std::cout << "Press 3 to print he final frade" << std::endl;
+        std::cout << "Press 3 to print the final grade" << std::endl;
         std::cout << "Press 4 to print minimum grade needed in a category based on a desired grade." << std::endl;
         std::cout << "Press 5 to print a final grade based on inputed category grades." << std::endl;
-        //std::cout << "Press 6 to add an entry." << std::endl;
-        std::cout << "Press 6 to add/modify a grade." << std::endl;
-        std::cout << "Press 7 to remove a grade." << std::endl;
-        std::cout << "Press 8 to load a file." << std::endl;
-        std::cout << "Press 9 to save as a different file." << std::endl;
-        std::cout << "Press 10 to add a new entry to your gradebook." << std::endl;
+        std::cout << "Press 6 to add a new entry to your gradebook." << std::endl;
+        std::cout << "Press 7 to add/modify a grade." << std::endl;
+        std::cout << "Press 8 to remove a grade." << std::endl;
+        std::cout << "Press 9 to load a file." << std::endl;
+        std::cout << "Press 10 to save as a different file." << std::endl;
         std::cout << "Press 0 to quit" << std::endl;
         std::cout << "Please Make a selection:";
         std::cin >> input;
@@ -56,30 +55,29 @@ bool takeInput(int * input, Gradebook *gradebook){
             gradebook->whatIfReport();
             break;
         case 6:
-            (*gradebook).addGrade();
+            (*gradebook).addEntry();
             break;
         case 7:
-            (*gradebook).removeGrade();
+            (*gradebook).addGrade();
             break;
         case 8:
-            // std::cout << "Please enter filename:" << std::endl;
-            // std::cin >> input_filename;
-            // (*gradebook).loadNewFile(input_filename);
-            (*gradebook).addEntry();
+            (*gradebook).removeGrade();
             break;
         case 9:
             std::cout << "Please enter filename:" << std::endl;
             std::cin >> input_filename;
-            (*gradebook).saveAs(input_filename);
+            (*gradebook).loadNewFile(input_filename);
             break;
         case 10:
-            (*gradebook).addEntry();
+            std::cout << "Please enter filename:" << std::endl;
+            std::cin >> input_filename;
+            (*gradebook).saveAs(input_filename);
             break;
         case 0:
             return false;
             break;
         default:
-            std::cout << "Your input is invalid, Please press 1, 2, 3, 4, 5, 6, 7, or 0." << std::endl;
+            std::cout << "Your input is invalid, Please press 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, or 0." << std::endl;
             break;
 
     }
